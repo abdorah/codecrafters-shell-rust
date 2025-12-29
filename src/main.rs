7ce8cfd5bbdedcb2acd5212ce5a1b37ac7a3cecd
Kotbi Abderrahmane <kotbymo@gmail.com>
2025-12-29 12:34:03 +0100

    test stage 2

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ use std::io::{self, Write};
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
     println!("$ ");
+    io::stdout().flush().unwrap();
     let stdin = io::read_to_string(io::stdin())?;
     println!("{}: command not found", stdin);
-    io::stdout().flush().unwrap();
+    Ok(())
 }
