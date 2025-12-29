2cb621973ebef4629e049e1549b6085d6ebf8ccd
Kotbi Abderrahmane <kotbymo@gmail.com>
2025-12-29 15:44:45 +0100

    test stage 3

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,59 @@
-#[allow(unused_imports)]
 use std::io::{self, Write};
 
+#[derive(Debug)]
+struct Command {
+    prompt: String,
+}
+
+impl Command {
+    fn new() -> Self {
+        Command {
+            prompt: String::new(),
+        }
+    }
+}
+
+trait Repl {
+    fn read(&mut self);
+    fn eval(&self);
+    fn print_prompt(&self);
+    fn run(&mut self);
+}
+
+impl Repl for Command {
+    fn print_prompt(&self) {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+    }
+
+    fn read(&mut self) {
+        self.prompt.clear();
+        io::stdin().read_line(&mut self.prompt).unwrap();
+    }
+
+    fn eval(&self) {
+        let message = self.prompt.trim();
+        if message.is_empty() {
+            return;
+        }
+        println!("{}: command not found", message);
+    }
+
+    fn run(&mut self) {
+        loop {
+            self.print_prompt();
+            self.read();
+
+            if self.prompt.trim() == "exit" {
+                break;
+            }
+
+            self.eval();
+        }
+    }
+}
+
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
-    let mut message = String::new();
-    io::stdin().read_line(&mut message).unwrap();
-    let message = message.trim();
-    println!("{}: command not found", message);
+    let mut command = Command::new();
+    command.run();
 }
