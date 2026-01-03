4348c16ffc8499c75a9b1295812f25ceb203dd8c
Kotbi Abderrahmane <kotbymo@gmail.com>
2026-01-03 11:11:52 +0100

    test stage 26

--- src/main.rs
+++ src/main.rs
@@ -518,9 +518,8 @@ impl Shell {
         if let Some((start, end, word)) = self.editor.get_word_at_cursor() {
             let completions = self.find_completions(word);
             let common = Self::common_prefix(&completions);
-            self.editor.replace_word(start, end, &common);
-            self.show_completions(&completions);
             self.redraw_line();
+            self.show_completions(&completions);
         }
     }
 
